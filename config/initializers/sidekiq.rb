7f08dd53f8aaa6e9409d1ba628c9aaccb659037a
Thiago Cruz <tcruz@horizonte.tv.br>
2017-04-29 01:57:39 -0300

    refacto(sidekiq): Tentativa de configuracao do sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,16 +2,16 @@ REDIS_CONN = proc do
   host =     ENV['REDIS_HOST']
   port =     ENV['REDIS_PORT']
 
-  redis_ready = host.present? and port.present?
+  # redis_ready = host.present? and port.present?
+  #
+  # redis_url = if redis_ready
+  #               "redis://#{host}:#{port}/"
+  #             else
+  #               "redis://localhost:6379/"
+  #             end
 
-  redis_url = if redis_ready
-                "redis://#{host}:#{port}/"
-              else
-                "redis://localhost:6379/"
-              end
-
-  r = Redis.new(url: redis_url)
-  Redis::Namespace.new("sidekiq", redis: r)
+  # r = Redis.new(url: redis_url)
+  # Redis::Namespace.new("sidekiq", redis: r)
 end
 
 Sidekiq.configure_client do |config|
