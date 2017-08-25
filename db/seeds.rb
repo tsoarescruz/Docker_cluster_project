05df8ec4716bf91ebef7bd12abc5d434fe3277b1
Thiago Cruz <tcruz@horizonte.tv.br>
2017-08-24 21:17:05 -0300

    refactor(db/seeds.rb): Alterando o email e password do admin do arquivo seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: 'Admin', email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+User.create!(name: 'Admin', email: 'admin@admin.com', password: 'admin', password_confirmation: 'admin')
 
 empresa = Tag.create!(name: 'empresa')
 ensino = Tag.create!(name: 'ensino')
