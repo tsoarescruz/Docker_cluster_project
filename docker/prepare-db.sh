c9747cda55806c3766d47af997e0dabcdf4c1cc1
Thiago Cruz <tcruz@horizonte.tv.br>
2017-12-06 07:16:03 -0200

    refactor(docker/scaffold/prepare-db): Alterando os scripts para a versao antiga, para o arm ja ha scripts na pasta que eu rodo daquele jeito

--- docker/prepare-db.sh
+++ docker/prepare-db.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # If database exists, migrate. Otherweise create and seed
-#docker-compose run --rm app rake db:reset
-docker-compose run --rm app bash -lc 'rake db:reset'
+docker-compose run --rm app rake db:reset
+#docker-compose run --rm app bash -lc 'rake db:reset'
 #echo "Done!"
